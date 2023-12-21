e932c0adf63e92ba4833d393068cf777b7cf49bf
Nabil Alfaatir <nabil_alfaatir09@yahoo.com>
2023-12-21 19:43:27 +0700

    Pengumpulan tugas 9

---
+++ sanberappflutter/lib/Tugas/Tugas9/Telegram.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'DrawerScreen.dart';
+import '../Tugas9/models/Chart_model.dart';
+
+class Telegram extends StatefulWidget {
+  const Telegram({super.key});
+
+  @override
+  State<Telegram> createState() => _TelegramState();
+}
+
+class _TelegramState extends State<Telegram> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Telegram"),
+        actions: const <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      drawer: const DrawerScreen(),
+      body: ListView.separated(
+          itemBuilder: ((context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 28,
+                backgroundImage: NetworkImage(items[index].profileUrl!),
+              ),
+              title: Text(
+                items[index].name!,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(items[index].message!),
+              trailing: Text(items[index].time!),
+            );
+          }),
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },
+          itemCount: items.length),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xFF65a9e0),
+        child: const Icon(
+          Icons.create,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
